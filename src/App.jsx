ea56c50b3b3024e92a5c0af6dc7e8ac21b32416b
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-12 15:55:37 +0600

    init with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-slate-800  text-white">
+      <h2 className="text-4xl text-center pt-2.5">Queue Management system</h2>
+      <p className="text-xl text-center text-gray-400">
+        Manage your queue efficiently
+      </p>
+    </div>
+  );
+};
+export default App;
